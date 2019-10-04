5387ebde32b1662ea4c59c13d487fdbe414e8b22
CTurner <cturner1@mymail.mines.edu>
2019-10-04 10:37:23 -0600

    Added IntBoard class

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -1,5 +1,35 @@
 package experiment;
 
+import java.util.Map;
+import java.util.Set;
+
 public class IntBoard {
 
+	private Map<BoardCell, Set<BoardCell>> adjacencies;
+	private Set<BoardCell> visited;
+	private Set<BoardCell> targets;
+	private BoardCell[][] grid;
+	
+	IntBoard() {
+		calcAdjacencies();
+	}
+	
+	void calcAdjacencies() {
+		
+	}
+	
+	Set<BoardCell> getAdjList(){
+		return null;
+	}
+	
+	void calcTargets(BoardCell startCell, int pathLength){
+		
+	}
+	
+	Set<BoardCell> getTargets(){
+		return null;
+	}
+	
+	
+	
 }
