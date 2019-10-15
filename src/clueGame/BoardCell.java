637e7aacc7f7e73f6fc9af0d6ff42af3eebf19af
CTurner <cturner1@mymail.mines.edu>
2019-10-14 19:28:41 -0600

    Part 0: imported all tests and input files

---
+++ src/clueGame/BoardCell.java
@@ -0,0 +1,18 @@
+package clueGame;
+
+/*
+ * Board Cell
+ * Casey Turner, Murat Tuter
+ * 
+ */
+public class BoardCell {
+
+	public int row;
+	public int column;
+	
+	public BoardCell(int r, int c) {
+		row = r;
+		column = c;
+	}
+
+}
