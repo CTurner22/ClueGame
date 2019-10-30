1ff8ecc6a35e560782ea477103763d762b64c8c4
CTurner <cturner1@mymail.mines.edu>
2019-10-29 22:33:37 -0600

    updated the solution class on git

---
+++ src/clueGame/Solution.java
@@ -0,0 +1,37 @@
+package clueGame;
+/*
+ * The solution class to hold the final awnser
+ * Casey Turner, Murat Tuter
+ * 
+ */
+public class Solution {
+
+	private Card murderer;
+	private Card crimeScene;
+	private Card murderWeapon;
+	
+	
+	public Solution(Card murderer, Card crimeScene, Card murderWeapon) {
+		assert(murderer.getType() == CardType.PERSON);
+		assert(crimeScene.getType() == CardType.ROOM);
+		assert(murderWeapon.getType() == CardType.WEAPON);
+
+		this.murderer = murderer;
+		this.crimeScene = crimeScene;
+		this.murderWeapon = murderWeapon;
+	}
+	
+	public Card getMurderer() {
+		return murderer;
+	}
+
+
+	public Card getCrimeScene() {
+		return crimeScene;
+	}
+
+
+	public Card getMurderWeapon() {
+		return murderWeapon;
+	}
+}
